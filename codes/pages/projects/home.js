1ca65ce005949ccddbd85838947dd4b52df7b807
DIR\m.garcia.paredes <m.garcia.paredes@accenture.com>
2023-05-03 13:08:46 +0200

    no message

---
+++ codes/pages/projects/home.js
@@ -0,0 +1,9 @@
+function show_popup(){
+    let popup = document.getElementById("popup");
+    popup.style.visibility = "visible";
+}
+
+function close_popup(){
+    let popup = document.getElementById("popup");
+    popup.style.visibility = "hidden";
+}
