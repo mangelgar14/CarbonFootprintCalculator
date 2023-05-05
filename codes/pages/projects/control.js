3c7216881522344ee376b26cf6580e5297df2bf4
DIR\mario.garcia.perez <mario.garcia.perez@accenture.com>
2023-05-05 10:57:48 +0200

    Projects Update

--- codes/pages/projects/control.js
+++ codes/pages/projects/control.js
@@ -4,13 +4,13 @@ window.onload = loader();
 function loader() {
     let listProjects = document.querySelectorAll('.project-card');
     listProjects.forEach(listProject => {
-        listProject.addEventListener('click', () => {
+        listProject.querySelectorAll('.project-card-preview')[0].addEventListener('click', () => {
             if (project != listProject) {
                 closed = false;
                 if (project != null) {
 
                     project.querySelectorAll('.project-card-arrow')[0].classList.remove('arrow');
-                    project.querySelectorAll('.project-card-details')[0].classList.add('hidden');
+                    project.querySelectorAll('.project-card-details')[0].classList.toggle('hidden');
                 }
             } else {
                 closed = !closed;
@@ -28,12 +28,10 @@ function addControl(closed) {
     if (closed) {
         document.getElementById('add_version_button').disabled = true;
         document.getElementById('add_project_button').disabled = false;
-        document.getElementById('add_version_button').classList.add('disabled');
-        document.getElementById('add_project_button').classList.remove('disabled');
+
     } else {
         document.getElementById('add_version_button').disabled = false;
         document.getElementById('add_project_button').disabled = true;
-        document.getElementById('add_version_button').classList.remove('disabled');
-        document.getElementById('add_project_button').classList.add('disabled');
+
     }
 }
